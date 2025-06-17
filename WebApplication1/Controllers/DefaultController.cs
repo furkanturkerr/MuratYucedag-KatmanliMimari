b20cc655067cfc82f7a7a075b198284afe74b498
Furkan TÜRKER <furkanturker.dev@gmail.com>
2025-06-18 01:14:10 +0300

    Metodlarda Parametre Kullanımı

--- WebApplication1/Controllers/DefaultController.cs
+++ WebApplication1/Controllers/DefaultController.cs
@@ -8,14 +8,36 @@ public class DefaultController : Controller
     {
         ViewBag.Mesaj = "Merhaba";
     }
+
+    void MesajListesi(string p)
+    {
+        ViewBag.v = p;
+    }
+
+    void Kullanıcı(string kullanici)
+    {
+        ViewBag.kullanici = kullanici;
+    }
+
+    int topla()
+    {
+        int s1 = 10;
+        int s2 = 20;
+        int sonuc = s1 + s2;
+        return sonuc;
+    }
     public IActionResult Index()
     {
         Mesajlar();
+        MesajListesi("Merhaba");
+        Kullanıcı("Murat123");
         return View();
     }
 
     public IActionResult Urunler()
     {
+        ViewBag.urun = topla();
+        Kullanıcı("Ahmet123");
         return View();
     }
 }
