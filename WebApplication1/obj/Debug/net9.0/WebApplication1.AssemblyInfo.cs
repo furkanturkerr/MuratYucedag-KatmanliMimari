b1ecc0499aeedf2a0ecc815d5f18aab2ed6e2a2c
Furkan TÜRKER <furkanturker.dev@gmail.com>
2025-06-29 01:53:22 +0300

    Ürün Ekleme - Silme

--- WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a53e40445a2f518d4ff60976ffafb2cea8a9793")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b71b74fb9027f970d4b7f0a45a6699ec0a70da1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
