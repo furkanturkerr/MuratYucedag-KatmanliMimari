8a53e40445a2f518d4ff60976ffafb2cea8a9793
Furkan TÜRKER <furkanturker.dev@gmail.com>
2025-06-28 20:18:00 +0300

    Bölüm-3

--- WebApplication1/Controllers/DefaultController.cs
+++ WebApplication1/Controllers/DefaultController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Ornekler;
 
 namespace WebApplication1.Controllers;
 
@@ -38,6 +39,43 @@ public class DefaultController : Controller
     {
         ViewBag.urun = topla();
         Kullanıcı("Ahmet123");
+        return View();
+    }
+    
+    public IActionResult Deneme()
+    {
+        Sehirler sehirler = new Sehirler();
+        
+        sehirler.Ad = "Ankara";
+        sehirler.Id = "1";
+        sehirler.Nufus = 1000000;
+        sehirler.Ulke = "Türkiye";
+        sehirler.Renk1 = "Mavi";
+        sehirler.Renk2 = "Sarı";
+        
+        ViewBag.v3 = sehirler.Ad;
+        ViewBag.v1 = sehirler.Id;
+        ViewBag.v4 = sehirler.Nufus;
+        ViewBag.v2 = sehirler.Ulke;
+        ViewBag.v5 = sehirler.Renk1;
+        ViewBag.v6 = sehirler.Renk2;
+        ViewBag.v7 = sehirler.Renk3;    
+        
+        sehirler.Ad = "İstanbul";
+        sehirler.Id = "2";
+        sehirler.Nufus = 20000000;
+        sehirler.Ulke = "Türkiye";
+        sehirler.Renk1 = "Kırmızı";
+        sehirler.Renk2 = "Sarı";
+        
+        ViewBag.z3 = sehirler.Ad;
+        ViewBag.z1 = sehirler.Id;
+        ViewBag.z4 = sehirler.Nufus;
+        ViewBag.z2 = sehirler.Ulke;
+        ViewBag.z5 = sehirler.Renk1;
+        ViewBag.z6 = sehirler.Renk2;
+        
+        
         return View();
     }
 }
---
+++ WebApplication1/Ornekler/Bayrak.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Ornekler;
+
+public class Bayrak
+{
+    public string Renk1 { get; set; }
+    public string Renk2 { get; set; }
+    public string Renk3 { get; set; }
+}
