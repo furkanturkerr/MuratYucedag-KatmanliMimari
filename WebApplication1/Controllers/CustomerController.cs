838b4834eabb7c1ed2b24f05f5ccdda24ba1054b
Furkan TÜRKER <furkanturker.dev@gmail.com>
2025-07-05 20:57:10 +0300

    Sürüm

---
+++ WebApplication1/Controllers/CustomerController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Entity;
+using WebApplication1.ProjeContext;
+
+namespace WebApplication1.Controllers;
+
+public class CustomerController : Controller
+{
+    Context context = new Context();
+    // GET
+    public IActionResult Index()
+    {
+        var values = context.Customers.ToList();
+        return View(values);
+    }
+    
+    [HttpGet]
+    public IActionResult AddCustomer()
+    { 
+        return View();
+    }
+    
+    [HttpPost]
+    public IActionResult AddCustomer(Customer customer)
+    {
+        if (customer.Name.Length > 1 && customer.City.Length > 1)
+        {
+            context.Add(customer);
+            context.SaveChanges();
+            TempData["SuccessMessage"] = "Müşteri başarıyla eklendi.";
+            return RedirectToAction("Index");
+        }
+        else
+        {
+            if (customer.Name.Length <= 1)
+            {
+                ViewBag.NameError = "Müşteri adı en az 2 karakter olmalıdır.";
+            }
+            if (customer.City.Length <= 1)
+            {
+                ViewBag.CityError = "Şehir adı en az 2 karakter olmalıdır.";
+            }
+            return View(customer);
+        }
+
+    }
+    
+    public IActionResult DeleteCustomer(int id)
+    {
+        var value = context.Customers.Where(x=>x.Id == id).FirstOrDefault();
+        context.Remove(value);
+        context.SaveChanges();
+        return RedirectToAction("Index");
+    }
+    
+    [HttpGet]
+    public IActionResult UpdateCustomer(int id)
+    {
+        var value = context.Customers.Where(x=>x.Id == id).FirstOrDefault();
+        return View(value);
+    }
+
+    [HttpPost]
+    public IActionResult UpdateCustomer(Customer customer)
+    {
+        var value = context.Customers.Where(x => x.Id == customer.Id).FirstOrDefault();
+        value.Name = customer.Name;
+        value.City = customer.City;
+        context.SaveChanges();
+        return RedirectToAction("Index");
+    }
+}
--- WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b5e2ec2ed30fca11951c795f6ddf94c6b7d0247")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ea25978a628dd9ba5bef73d3b587bb2b96ae8c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
