cb6cf59ee9e060696a1c28516587e2c458dc77a2
Furkan Türker <furkanturker.dev@gmail.com>
2025-07-06 13:48:31 +0300

    aa

---
+++ Proje - 1/WebApplication1/ProjeContext/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Entity;
+
+namespace WebApplication1.ProjeContext
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-2QRUQ1N\\MSSQLSERVER01;Database=MuratYucedag; Integrated Security = True; TrustServerCertificate = True");
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
