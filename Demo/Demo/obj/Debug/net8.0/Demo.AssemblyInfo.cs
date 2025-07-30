a69862cc5977dc7a6f0b9dd3c8e39b982982a6ba
Furkan Türker <furkanturker.dev@gmail.com>
2025-07-30 22:34:26 +0300

    Identity Kütüphanesi Eklendi

--- Demo/Demo/obj/Debug/net8.0/Demo.AssemblyInfo.cs
+++ Demo/Demo/obj/Debug/net8.0/Demo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+414efb2bc7e1eedf2316269df3aaab35803fd348")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69442f33ea1572422be62e00e6141beb1d97f99d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
