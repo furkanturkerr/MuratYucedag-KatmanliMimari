81dd2527b13efa9df7e6f0f6894e1c66180a0f68
Furkan Türker <furkanturker.dev@gmail.com>
2025-10-23 01:44:26 +0300

    Klonlama

--- Demo/BusinessLayer/obj/Debug/net8.0/BusinessLayer.AssemblyInfo.cs
+++ Demo/BusinessLayer/obj/Debug/net8.0/BusinessLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+291cd1b9382b8243a6080b890f97b2f2cab1372d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+370bea2208b8daf9def127c9ac3689e8d8ef58ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Demo/DataAccessLayer/obj/Debug/net8.0/DataAccessLayer.AssemblyInfo.cs
+++ Demo/DataAccessLayer/obj/Debug/net8.0/DataAccessLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e43c18e4eafcbeab92ae49867c39029fda5a2cae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+370bea2208b8daf9def127c9ac3689e8d8ef58ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
